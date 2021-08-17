c466c33a0829f9973b09db3fe4a14a8b278b6ad0
Yuurga <yuurga@gmail.com>
2021-08-17 12:30:44 +0300

    add create table test

--- src/test/scala/ru/otus/spark/sales/HiveMetastoreTest.scala
+++ src/test/scala/ru/otus/spark/sales/HiveMetastoreTest.scala
@@ -1,9 +1,36 @@
 package ru.otus.spark.sales
 
-import org.apache.spark.sql.SaveMode
+import org.apache.spark.sql.{Row, SaveMode}
 import org.scalatest.FunSuite
 
 class HiveMetastoreTest extends FunSuite with SparkSessionTestWrapper {
+
+  override def beforeAll() {
+    super.beforeAll()
+    //ss.sqlContext.sql("drop table person")
+  }
+
+  test("Test table creation and summing of counts") {
+    val personRDD = ss.sparkContext.parallelize(Seq(Row("Anna", 25, "manager"),
+      Row("Andrew", 35, "devOps"),
+      Row("Alexander", 19, "Junior")))
+
+    ss.sqlContext.sql("create table person (name string, age int, position string)")
+
+    val emptyDataFrame = ss.sqlContext.sql("select * from person limit 0")
+
+    val personDataFrame = ss.sqlContext.createDataFrame(personRDD, emptyDataFrame.schema)
+    personDataFrame.createOrReplaceTempView("tempPerson")
+
+    val ageSumDataFrame = ss.sqlContext.sql("select sum(age) from tempPerson")
+
+    personDataFrame.write.mode("overwrite").insertInto("person")
+
+    val localAgeSum = ageSumDataFrame.take(10)
+
+    assert(localAgeSum(0).get(0) == 79, "The sum of age should equal 62 but it equaled " + localAgeSum(0).get(0))
+  }
+
 
   test("Save dataframe with final fantasy and load from table all 89 records") {
     val df = ss.read
